a53a3d9fcead1c285166b6ceddc4a2f83b8c0e90
Christian Lisangola <jochri3@gmail.com>
2023-10-17 12:13:05 +0300

    Point 2 : Getters, setters et construteurs

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,17 @@
 public class Main {
     public static void main(String[] args) {
        Rectangle monRec;
-       monRec=new Rectangle();
+       monRec=new Rectangle(500,250);
 
-       monRec.longueur=500;
-       monRec.largeur=250;
 
-       System.out.printf("Surface : %.2f\n",monRec.calculeSurface());
-       System.out.printf("Perimètre : %.2f",monRec.calculPerimetre());
+//       monRec.setLongueur(500);
+//       monRec.setLargeur(250);
+
+        System.out.printf("Longueur : %.2f\n",monRec.getLongueur());
+        System.out.printf("largeur : %.2f\n\n",monRec.getLargeur());
+//
+//       System.out.printf("Surface : %.2f\n",monRec.calculeSurface());
+//       System.out.printf("Perimètre : %.2f",monRec.calculPerimetre());
 
     }
 }
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,16 +1,42 @@
 public class Rectangle {
 
     // Variables membres = attributs = propriétes
-    double longueur;
-    double largeur;
+    private double longueur;
+    private double largeur;
 
+    public Rectangle(double longueur,double largeur){
+        this.longueur=longueur;
+        this.largeur=largeur;
+    }
 
     // Fonctions membres = méthodes
-    double calculeSurface(){
+    public double calculeSurface(){
         return longueur*largeur;
     }
 
-    double calculPerimetre(){
+    public double calculPerimetre(){
         return  2*(longueur+largeur);
+    }
+
+    // setters : Méthodes publiques pour modifier des attributs
+    public void setLongueur(double longueur){
+        if(longueur>=0){
+            this.longueur=longueur;// shadowing
+        }
+    }
+
+    public void setLargeur(double largeur){
+       if(largeur>=0){
+           this.largeur=largeur;
+       }
+    }
+
+    // getters: Méthodes publiques pour lire des attributs
+    public double getLongueur(){
+        return longueur;
+    }
+
+    public double getLargeur(){
+        return largeur;
     }
 }
